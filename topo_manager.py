4651d568bfc57679f4e6c1069463711b6a8cfe0a
vagrant <vagrant@comnetsemu>
2023-08-02 14:24:39 +0000

    changes to topology_manager:added dictionaries for dpid

--- topo_manager.py
+++ topo_manager.py
@@ -4,6 +4,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ofctl_utils import OfCtl, packet,ethernet, arp 
 from ryu.lib.packet import ether_types
+import networkx as nx
 
 
 class Device():
@@ -89,22 +90,42 @@ class TopoManager():
         # Initialize some data structures
         self.all_devices = []
         self.all_links = []
+        self.network_graph = nx.Graph()
+        self.topo = {}
+        self.host_locate = {}
+
+    
+
+    @set_ev_cls(event.EventSwitchEnter)
+    def handle_switch_enter(self, ev):
+        switch = ev.switch
+        self.add_switch(switch)
+
+    @set_ev_cls(event.EventLinkAdd)
+    def handle_link_add(self, ev):
+        link = ev.link
+        self.add_link(link)
+
+    @set_ev_cls(event.EventHostAdd)
+    def handle_host_add(self, ev):
+        host = ev.host
+        self.add_host(host)
 
     def add_switch(self, sw):
         name = "switch_{}".format(sw.dp.id)
         switch = TMSwitch(name, sw)
 
         self.all_devices.append(switch)
-
-        # TODO: Add switch to some data structure(s)
+        self.network_graph.add_node(switch.dp.id)
+        self.topo[str(sw.dp.id)] = {}
 
     def add_host(self, h):
         name = "host_{}".format(h.mac)
         host = TMHost(name, h)
 
         self.all_devices.append(host)
-
-        # TODO: Add host to some data structure(s)
+        self.network_graph.add_node(host.mac)
+        self.host_locate[str(h.port.dpid)] = {h.mac}
 
     def add_link(self, link):
         src_dev = self.get_device_by_port(link.src)
@@ -114,7 +135,11 @@ class TopoManager():
             src_dev.add_neighbor(dst_dev)
             dst_dev.add_neighbor(src_dev)
 
-        # Add link to some data structure(s)
+        src_switch = link.src.dpid
+        dst_switch = link.dst.dpid
+        self.network_graph.add_edge(src_switch, dst_switch)
+        self.topo[str(src_switch)][str(dst_switch)] = 1
+        self.topo[str(dst_switch)][str(src_switch)] = 1
 
     def remove_link(self, link):
         src_dev = self.get_device_by_port(link.src)
@@ -133,117 +158,15 @@ class TopoManager():
             elif isinstance(dev, TMHost) and port == dev.get_port():
                 return dev
 
-    @set_ev_cls(event.EventSwitchEnter)
-    def handle_switch_add(self, ev):
-        """
-        Event handler indicating a switch has come online.
-        """
-        switch = ev.switch
-
-        self.logger.warn("Added Switch switch%d with ports:", switch.dp.id)
-        for port in switch.ports:
-            self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
-
-        self.add_switch(switch)
-
-    @set_ev_cls(event.EventSwitchLeave)
-    def handle_switch_delete(self, ev):
-        """
-        Event handler indicating a switch has been removed
-        """
-        switch = ev.switch
-
-        self.logger.warn("Removed Switch switch%d with ports:", switch.dp.id)
-        for port in switch.ports:
-            self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
-
-        # TODO: Update network topology and flow rules
-
-    @set_ev_cls(event.EventHostAdd)
-    def handle_host_add(self, ev):
-        """
-        Event handler indicating a host has joined the network
-        This handler is automatically triggered when a host sends an ARP response.
-        """
-        host = ev.host
-        self.logger.warn("Host Added:  %s (IPs:  %s) on switch%s/%s (%s)",
-                          host.mac, host.ipv4,
-                          host.port.dpid, host.port.port_no, host.port.hw_addr)
-
-        self.add_host(host)
-
-    @set_ev_cls(event.EventLinkAdd)
-    def handle_link_add(self, ev):
-        """
-        Event handler indicating a link between two switches has been added
-        """
-        link = ev.link
-        src_port = link.src
-        dst_port = link.dst
-        self.logger.warn("Added Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
-                         src_port.dpid, src_port.port_no, src_port.hw_addr,
-                         dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
-
-        self.add_link(link)
-
-    @set_ev_cls(event.EventLinkDelete)
-    def handle_link_delete(self, ev):
-        """
-        Event handler indicating when a link between two switches has been deleted
-        """
-        link = ev.link
-        src_port = link.src
-        dst_port = link.dst
-
-        self.logger.warn("Deleted Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
-                          src_port.dpid, src_port.port_no, src_port.hw_addr,
-                          dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
-
-        self.remove_link(link)
-
-    @set_ev_cls(event.EventPortModify)
-    def handle_port_modify(self, ev):
-        """
-        Event handler for when any switch port changes state.
-        This includes links for hosts as well as links between switches.
-        """
-        port = ev.port
-        self.logger.warn("Port Changed:  switch%s/%s (%s):  %s",
-                         port.dpid, port.port_no, port.hw_addr,
-                         "UP" if port.is_live() else "DOWN")
-
-        # TODO: Update network topology and flow rules
-
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        """
-       EventHandler for PacketIn messages
-        """
-        msg = ev.msg
-
-        # In OpenFlow, switches are called "datapaths".  Each switch gets its own datapath ID.
-        # In the controller, we pass around datapath objects with metadata about each switch.
-        dp = msg.datapath
-
-        # Use this object to create packets for the given datapath
-        ofctl = OfCtl.factory(dp, self.logger)
-
-        in_port = msg.in_port
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        if eth.ethertype == ether_types.ETH_TYPE_ARP:
-            arp_msg = pkt.get_protocols(arp.arp)[0]
-
-            if arp_msg.opcode == arp.ARP_REQUEST:
-
-                self.logger.warning("Received ARP REQUEST on switch%d/%d:  Who has %s?  Tell %s",
-                                    dp.id, in_port, arp_msg.dst_ip, arp_msg.src_mac)
-
-                # TODO: Generate a *REPLY* for this request based on your switch state
-
-                # Here is an example way to send an ARP packet using the ofctl utilities
-                # ofctl.send_arp(vlan_id=VLANID_NONE,
-                #                src_port=ofctl.dp.ofproto.OFPP_CONTROLLER,
-                #                . . .)
-
+    def get_shortest_path(self, src_switch, dst_switch):
+        try:
+            shortest_path = nx.shortest_path(self.network_graph, source=src_switch, target=dst_switch)
+            return shortest_path
+        except nx.NetworkXNoPath:
+            return None
+        
+    def get_switch_by_dpid(self, dpid):
+        for dev in self.all_devices:
+            if isinstance(dev, TMSwitch) and dev.get_dpid() == dpid:
+                return dev
+        return None
