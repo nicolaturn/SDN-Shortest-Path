9ba53910a8ca6a5a009c6d261935df2b6187edac
vagrant <vagrant@comnetsemu>
2023-08-03 12:26:13 +0000

    fixed topology manager for switches and links

--- shortest_path.py
+++ shortest_path.py
@@ -14,7 +14,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_0, ofproto_v1_0_parser
 
 from ryu.topology import event, switches
 import ryu.topology.api as topo
@@ -25,6 +25,10 @@ from ryu.lib.packet import ethernet, arp, icmp
 from ofctl_utils import OfCtl, VLANID_NONE
 
 from topo_manager import TopoManager
+import logging
+
+
+
 
 
 class ShortestPathSwitching(app_manager.RyuApp):
@@ -34,6 +38,8 @@ class ShortestPathSwitching(app_manager.RyuApp):
         super(ShortestPathSwitching, self).__init__(*args, **kwargs)
 
         self.tm = TopoManager()
+        self.mac_to_port={}
+        logging.basicConfig(level=logging.DEBUG)
 
     @set_ev_cls(event.EventSwitchEnter)
     def handle_switch_add(self, ev):
@@ -61,6 +67,7 @@ class ShortestPathSwitching(app_manager.RyuApp):
             self.logger.warn("\t%d:  %s", port.port_no, port.hw_addr)
 
         # TODO:  Update network topology and flow rules
+        
 
     @set_ev_cls(event.EventHostAdd)
     def handle_host_add(self, ev):
@@ -68,6 +75,7 @@ class ShortestPathSwitching(app_manager.RyuApp):
         Event handler indicating a host has joined the network
         This handler is automatically triggered when a host sends an ARP response.
         """
+        self.logger.info("Handling EventHostAdds")
         host = ev.host
         self.logger.warn("Host Added:  %s (IPs:  %s) on switch%s/%s (%s)",
                           host.mac, host.ipv4,
@@ -81,14 +89,19 @@ class ShortestPathSwitching(app_manager.RyuApp):
         """
         Event handler indicating a link between two switches has been added
         """
+        self.logger.warn("adding link....")
         link = ev.link
-        src_port = ev.link.src
-        dst_port = ev.link.dst
-        self.logger.warn("Added Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
-                         src_port.dpid, src_port.port_no, src_port.hw_addr,
-                         dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
+        src_switch=link.src.dpid
+        src_port_no=link.src.port_no
+        dst_switch=link.dst.dpid
+        dst_port_no=link.dst.port_no
+        #self.logger.warn("Added Link:  switch%s/%s (%s) -> switch%s/%s (%s)",
+                         #src_port.dpid, src_port.port_no, src_port.hw_addr,
+                         #dst_port.dpid, dst_port.port_no, dst_port.hw_addr)
 
         # TODO:  Update network topology and flow rules
+        #self.logger.warn("link of type", type(link.src))
+        self.tm.add_link(src_switch,src_port_no,dst_switch,dst_port_no)
 
     @set_ev_cls(event.EventLinkDelete)
     def handle_link_delete(self, ev):
@@ -117,43 +130,86 @@ class ShortestPathSwitching(app_manager.RyuApp):
                          "UP" if port.is_live() else "DOWN")
 
         # TODO:  Update network topology and flow rules
+        
+
+
+
+# ... Existing code ...
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        """
-       EventHandler for PacketIn messages
-        """
+    def _packet_in_handler(self, ev):
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                            ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
-
-        # In OpenFlow, switches are called "datapaths".  Each switch gets its own datapath ID.
-        # In the controller, we pass around datapath objects with metadata about each switch.
-        dp = msg.datapath
-
-        # Use this object to create packets for the given datapath
-        ofctl = OfCtl.factory(dp, self.logger)
-
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
         in_port = msg.in_port
+
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        if eth.ethertype == ether_types.ETH_TYPE_ARP:
-            arp_msg = pkt.get_protocols(arp.arp)[0]
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # Ignore LLDP packet
+            return
 
-            if arp_msg.opcode == arp.ARP_REQUEST:
+        dst = eth.dst
+        src = eth.src
 
-                self.logger.warning("Received ARP REQUEST on switch%d/%d:  Who has %s?  Tell %s",
-                                    dp.id, in_port, arp_msg.dst_ip, arp_msg.src_mac)
+        dpid = datapath.id
+        src_dpid = self.tm.dpid_hostLookup(src)
+        dst_dpid = self.tm.dpid_hostLookup(dst)
+        self.mac_to_port.setdefault(dpid, {})
 
-                # TODO:  Generate a *REPLY* for this request based on your switch state
+        if dst_dpid is not None:
+            self.logger.info("\n\tpacket in %s %s %s %s %s", dpid, src, dst, dst_dpid, in_port)
 
-                # Here is an example way to send an ARP packet using the ofctl utilities
-                #ofctl.send_arp(vlan_id=VLANID_NONE,
-                #               src_port=ofctl.dp.ofproto.OFPP_CONTROLLER,
-                #               . . .)
+        # Learn a MAC address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Call the TopoManager to get the shortest path between source and destination switches
+        if str(src_dpid) in self.tm.topo and str(dst_dpid) in self.tm.topo:
+            self.logger.info("calling to dijkstra with source %s destination %s", src_dpid, dst_dpid)
+            path = self.tm.dijkstra(str(src_dpid), str(dst_dpid))
+
+            if path is not None:
+                out_port = self.tm.get_output_port(path, dpid)
+                if out_port is not None:
+                    actions = [parser.OFPActionOutput(out_port)]
+
+        # Install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # Verify if we have a valid buffer_id, if yes avoid sending both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
 
 
 if __name__ == '__main__':
-    from ryu.lib import hub
+    from ryu.cmd import main
 
     # Run the Ryu controller
     app_manager.run_eventlet(ShortestPathSwitching)
+    #ryu_app=ShortestPathSwitching()
+    #main([ryu_app])
--- topo_manager.py
+++ topo_manager.py
@@ -94,72 +94,73 @@ class TopoManager():
         self.topo = {}
         self.host_locate = {}
 
-    
-
-    @set_ev_cls(event.EventSwitchEnter)
-    def handle_switch_enter(self, ev):
-        switch = ev.switch
-        self.add_switch(switch)
-
-    @set_ev_cls(event.EventLinkAdd)
-    def handle_link_add(self, ev):
-        link = ev.link
-        self.add_link(link)
-
-    @set_ev_cls(event.EventHostAdd)
-    def handle_host_add(self, ev):
-        host = ev.host
-        self.add_host(host)
-
     def add_switch(self, sw):
         name = "switch_{}".format(sw.dp.id)
         switch = TMSwitch(name, sw)
 
         self.all_devices.append(switch)
-        self.network_graph.add_node(switch.dp.id)
+        self.network_graph.add_node(sw.dp.id)
         self.topo[str(sw.dp.id)] = {}
+        print("Added switch node to network_graph:", sw.dp.id)
+        print("Current network_graph nodes:",self.network_graph.nodes)
 
     def add_host(self, h):
+        print("initial phase of adding host")
         name = "host_{}".format(h.mac)
         host = TMHost(name, h)
+        print("adding host...",h)
 
         self.all_devices.append(host)
-        self.network_graph.add_node(host.mac)
+        self.network_graph.add_node(h.mac)
         self.host_locate[str(h.port.dpid)] = {h.mac}
 
-    def add_link(self, link):
-        src_dev = self.get_device_by_port(link.src)
-        dst_dev = self.get_device_by_port(link.dst)
+    def add_link(self, src_switch, src_port_no, dst_switch, dst_port_no):
+        src_dev = self.get_device_by_port(src_switch, src_port_no)
+        dst_dev = self.get_device_by_port(dst_switch, dst_port_no)
 
-        if src_dev and dst_dev:
+        if src_dev and dst_dev and isinstance(src_dev, TMSwitch) and isinstance(dst_dev, TMSwitch):
             src_dev.add_neighbor(dst_dev)
             dst_dev.add_neighbor(src_dev)
 
-        src_switch = link.src.dpid
-        dst_switch = link.dst.dpid
         self.network_graph.add_edge(src_switch, dst_switch)
         self.topo[str(src_switch)][str(dst_switch)] = 1
         self.topo[str(dst_switch)][str(src_switch)] = 1
+
+        print("Added link edge to network_graph:", src_switch, "->", dst_switch)
+        print("Current network_graph edges:", self.network_graph.edges)
+
+
+        
 
     def remove_link(self, link):
         src_dev = self.get_device_by_port(link.src)
         dst_dev = self.get_device_by_port(link.dst)
 
-        if src_dev and dst_dev:
+        if src_dev and dst_dev and isinstance(src_dev,TMSwitch) and isinstance(dst_dev,TMSwitch):
             src_dev.neighbors.remove(dst_dev)
             dst_dev.neighbors.remove(src_dev)
+            src_switch=link.src.dpid
+            dst_switch=link.dst.dpid
+            self.network_graph.remove_edge(src_switch,dst_switch)
 
         # Remove link from data structure(s)
 
-    def get_device_by_port(self, port):
+    def get_device_by_port(self, dpid, port_no):
         for dev in self.all_devices:
-            if isinstance(dev, TMSwitch) and port in dev.get_ports():
+            if isinstance(dev, TMSwitch):
+                for port in dev.get_ports():
+                    if port.port_no == port_no and dev.get_dpid() == dpid:
+                        return dev
+            elif isinstance(dev, TMHost) and port_no == dev.get_port().port_no:
                 return dev
-            elif isinstance(dev, TMHost) and port == dev.get_port():
-                return dev
+        return None
+
+
 
     def get_shortest_path(self, src_switch, dst_switch):
         try:
+            src_switch=str(src_switch)
+            dst_switch=str(dst_switch)
             shortest_path = nx.shortest_path(self.network_graph, source=src_switch, target=dst_switch)
             return shortest_path
         except nx.NetworkXNoPath:
@@ -169,4 +170,114 @@ class TopoManager():
         for dev in self.all_devices:
             if isinstance(dev, TMSwitch) and dev.get_dpid() == dpid:
                 return dev
+        return None
+    
+    def dijkstra(self, src_switch, dst_switch):
+        # Initialize distance and previous nodes
+        dist = {switch: float('inf') for switch in self.topo}
+        prev = {switch: None for switch in self.topo}
+
+        # Distance to the source switch is 0
+        dist[src_switch] = 0
+
+        # Set of unvisited switches
+        unvisited = set(self.topo.keys())
+
+        while unvisited:
+            # Find the unvisited switch with the minimum distance
+            current_switch = min(unvisited, key=lambda switch: dist[switch])
+
+            # Remove the current switch from the unvisited set
+            unvisited.remove(current_switch)
+
+            # If the destination switch has been reached, break the loop
+            if current_switch == dst_switch:
+                break
+
+            # Update the distance and previous nodes for neighboring switches
+            for neighbor_switch, cost in self.topo[current_switch].items():
+                new_distance = dist[current_switch] + cost
+                if new_distance < dist[neighbor_switch]:
+                    dist[neighbor_switch] = new_distance
+                    prev[neighbor_switch] = current_switch
+
+        # Build and return the shortest path
+        shortest_path = []
+        current_switch = dst_switch
+        while current_switch is not None:
+            shortest_path.append(current_switch)
+            current_switch = prev[current_switch]
+
+        shortest_path.reverse()
+        return shortest_path
+    
+    def dpid_hostLookup(self, mac):
+        for host in self.all_devices:
+            if isinstance(host, TMHost) and host.get_mac() == mac:
+                return host.get_port().dpid
+        return None
+    
+    def forward_packet(self, datapath, in_port, pkt, eth_pkt):
+        src_mac = eth_pkt.src
+        dst_mac = eth_pkt.dst
+
+        src_switch = datapath.id
+
+        # If the destination MAC is known in the same switch, send the packet directly
+        for neighbor in self.get_device_by_port(in_port).neighbors:
+            if isinstance(neighbor, TMHost) and neighbor.get_mac() == dst_mac:
+                self.send_packet(datapath, in_port, pkt)
+                return
+
+        # If the destination MAC is not known in the same switch, calculate the shortest path
+        # to the destination and forward the packet accordingly
+        dst_switch = self.dpid_hostLookup(dst_mac)
+
+        if dst_switch is not None:
+            shortest_path = self.dijkstra(src_switch, dst_switch)
+
+            if shortest_path is not None and len(shortest_path) > 1:
+                next_hop_switch = shortest_path[1]
+                out_port = self.get_output_port(src_switch, next_hop_switch)
+
+                if out_port is not None:
+                    actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+                    self.send_packet_out(datapath, pkt, actions)
+
+
+    def send_packet_out(self, datapath, pkt, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = datapath.ofproto.OFPP_CONTROLLER
+        buffer_id = datapath.ofproto.OFP_NO_BUFFER
+        data = pkt.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=buffer_id,
+                                    in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def send_packet(self, datapath, out_port, pkt):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        pkt.serialize()
+        data = pkt.data
+        actions = [parser.OFPActionOutput(out_port, ofproto.OFPCML_NO_BUFFER)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=data)
+        datapath.send_msg(out)
+
+
+
+
+
+    def get_output_port(self, src_switch, dst_switch):
+        path = self.get_shortest_path(src_switch, dst_switch)
+        if path is not None and len(path) > 1:
+            src = path[0]
+            dst = path[1]
+            switch = self.get_switch_by_dpid(src)
+            if switch is not None:
+                for port in switch.get_ports():
+                    if port.dpid == dst:
+                        return port.port_no
         return None
