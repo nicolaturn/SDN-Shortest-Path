b63a0385d420a6f2328f4aa4eff5ef3ca706d06c
giuliomuraro <giuliomuraro@protonmail.com>
2023-08-17 16:09:04 +0000

    Merged code, resolved conflicts

--- run_mininet.py
+++ run_mininet.py
@@ -107,6 +107,39 @@ class MeshTopo(Topo):
             for j in range(i+1,n):
                 self.addLink(switches[i], switches[j])
 
+class LongPathTopo(Topo):
+    def __init__(self, **opts):
+        Topo.__init__(self, **opts)
+        h1= self.addHost('h1')
+        h2=self.addHost('h2')
+        h3=self.addHost('h3')
+        h4=self.addHost('h4')
+        s1=self.addSwitch('s1')
+        s2=self.addSwitch('s2')
+        s3=self.addSwitch('s3')
+        s4=self.addSwitch('s4')
+        s5=self.addSwitch('s5')
+        s6=self.addSwitch('s6')
+        s7=self.addSwitch('s7')
+        s8=self.addSwitch('s8')
+        s9=self.addSwitch('s9')
+        self.addLink(h1,s1)
+        self.addLink(h2,s2)
+        self.addLink(h3,s3)
+        self.addLink(h4,s4)
+        self.addLink(s1,s5)
+        self.addLink(s1,s8)
+        self.addLink(s1,s9)
+        self.addLink(s2,s5)
+        self.addLink(s2,s6)
+        self.addLink(s2,s9)
+        self.addLink(s3,s6)
+        self.addLink(s3,s9)
+        self.addLink(s3,s7)
+        self.addLink(s4,s7)
+        self.addLink(s4,s9)
+        self.addLink(s4,s8)
+
             
 
 
@@ -118,6 +151,7 @@ ALL_TOPOLOGIES = {
     "triangle": TriangleTopo,
     "mesh": MeshTopo,
     "someloops": SomeLoopsTopo,
+    "longpath" : LongPathTopo
 }
 
 
