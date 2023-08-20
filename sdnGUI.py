a9b4a4b58f02fd9bf8793035fd2b94e2befc908a
nicolaturn <nicola.turniano01@gmail.com>
2023-08-20 14:10:37 +0000

    terminali ok

--- sdnGUI.py
+++ sdnGUI.py
@@ -1,10 +1,14 @@
 ''' Graphic User Interface for the SDN '''
 
+#ryu-manager --observe-links shortest_path.py
+
 
 # Graphic modules
 import tkinter as tk
 from tkinter import ttk
 import os
+from time import sleep
+from matplotlib.backend_bases import FigureCanvasBase
 
 # Network graph modules
 import networkx as nx
@@ -18,16 +22,19 @@ from PIL import Image, ImageTk
 import socket
 import pickle
 import threading
+import subprocess
+
+
+
 
 # IP and port of Ryu App
 ipRyuApp = '127.0.0.1'
 portRyuApp = 6653
-
 # Global variable to receive the networkx.Graph
 graphReceived = False
 
 # Function to receive networkx.Graph from the Ryu App
-def receive_data_thread(graphLabel, outputTextBox):
+def receive_data_thread(graphLabel):
     guiSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     guiSocket.bind(ipRyuApp, portRyuApp)
     # The socket will wait for one connection at time
@@ -47,17 +54,18 @@ def receive_data_thread(graphLabel, outputTextBox):
         except pickle.UnpicklingError:
             received_object = None
 
-        if not graphReceived and isinstance(receivedGraph, networkx.Graph):
+        if not graphReceived and isinstance(receivedGraph, nx.Graph):
             # Create the graph and insert it into the GraphLabel
             create_graph_image(receivedGraph, graphLabel)
             graphReceived = True
-        elif isinstance(data, bytes):
+        elif isinstance(data, bytes): 
+            pass
             # Append the text logs to the text box for the logs of Ryu App
-            add_log(outputTextBox, data.decode())
+            #add_log(outputTextBox, data.decode())
 
 # Launch a xterminal with a defined width and height
-def launch_xterm(wid, width, height):
-    os.system(f'xterm -fa "Monospace" -fs 13 -into {wid} -geometry {width}x{height} -sb &')
+def launch_xterm(wid, width, height, strg):
+    os.system(f'xterm -fa "Monospace" -fs 13 -into {wid} -geometry {width}x{height} -sb -hold -e "{strg}"  &')
 
 
 # Keeping the text box for the Ryu output disable for the user, so it's a read-only logger
@@ -75,7 +83,7 @@ def create_graph_image(netGraph, graphLabel):
     
     # Create a Matplotlib figure and draw the graph on it
     fig = plt.figure(figsize=(5, 5))
-    canvas = FigureCanvas(fig)
+    canvas = FigureCanvasBase(fig)
     ax = fig.add_subplot(111)
     ax.set_axis_off()
     nx.draw(netGraph, pos, ax=ax, with_labels=True, node_size=500, font_size=10, font_color='black')
@@ -91,25 +99,46 @@ def create_graph_image(netGraph, graphLabel):
     # Update the label with the image
     graphLabel.config(image=photo)
     graphLabel.image = photo
+    print("label graph")
 
 # Function to start the thread for the logs management
-def start_listening_thread(graphLabel, outputTextBox):
-    parameters = (graphLabel, outputTextBox)
+def start_listening_thread(graphLabel):
+    parameters = (graphLabel)
 
     listening_thread = threading.Thread(target=receive_data_thread, args = parameters)
     listening_thread.daemon = True
     listening_thread.start()
 
+
+
+def button1():
+    launch_xterm(wid_output, 900, 300, "sudo mn -c && ryu-manager --observe-links shortest_path.py")
+    sleep(5)
+    launch_xterm(wid_mininet, 900, 300, "sudo python3 run_mininet.py triangle")
+
+def button2():
+    launch_xterm(wid_output, 900, 300, "sudo mn -c && ryu-manager --observe-links shortest_path.py")
+    sleep(5)
+    launch_xterm(wid_mininet, 900, 300, " sudo python3 run_mininet.py mesh 5")
+
+def button3():
+    launch_xterm(wid_output, 900, 300, "sudo mn -c && ryu-manager --observe-links shortest_path.py")
+    sleep(5)
+    launch_xterm(wid_mininet, 900, 300, " sudo python3 run_mininet.py longpath")
+        
+
 #Main function to execute the SDN GUI
 def main():
+    global wid_mininet
+    global wid_output
 
     ### Creating the main window
     window = tk.Tk()
     window.title("SDN viewer and tester")
 
     # Edit the dimensions of the main window
-    new_width = 1600
-    new_height = 1000
+    new_width = 1400
+    new_height = 800
     window.geometry(f"{new_width}x{new_height}")
 
     # The main window is not resizable and is not opened fullscreen
@@ -139,14 +168,14 @@ def main():
 
 
     ### Top left corner
-    graphFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 500, width = 800)
+    graphFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 400, width = 700)
     graphFrame.grid(row = 0, column = 0, sticky = 'nw')
 
     # The title of graph section
     titleLabelGraph = ttk.Label(graphFrame, text="Network Graph", background = "lightgray", font=("Helvetica", 14))
     titleLabelGraph.pack(side='top', padx = 10, pady = 5, fill = 'none', expand = False)
 
-    graphLabel = tk.Label(graphFrame, bg = "#C2C2A3", height = 400, width = 800)
+    graphLabel = tk.Label(graphFrame, bg = "#C2C2A3", height = 300, width = 700)
     graphLabel.pack(side = 'top', fill = 'none', expand = False)
 
     graphFrame.propagate(False)
@@ -154,24 +183,30 @@ def main():
 
 
     ### Top right corner
-    outputRyuFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 500, width = 800)
+    outputRyuFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 400, width = 700)
     outputRyuFrame.grid(row = 0, column = 1, sticky = 'ne')
 
     # The title of the outputTextBox of Ryu
     titleLabelOutput = ttk.Label(outputRyuFrame, text="Output Logger for Ryu controller", background = "lightgray", font=("Helvetica", 14))
     titleLabelOutput.pack(side='top', fill = 'none', expand = False, padx = 10, pady = 5)
 
-    outputRyuText = tk.Text(outputRyuFrame, bg = "#C2C2A3", state = 'disabled', height = 400, width = 800)
-    outputRyuText.pack(side = "top", fill = 'none', expand = False)
+    #outputRyuText = tk.Text(outputRyuFrame, bg = "#C2C2A3", height = 20, width = 10)
+    #outputRyuText.pack(side = "top", fill = 'none', expand = False)
     #add_log(outputRyuText, "Prova inserimento testo in text box")
+
+    termf2 = ttk.Frame(outputRyuFrame, height=400, width=700, style='RoundedFrame.TFrame')
+    termf2.pack(side='bottom')
+    wid_output = termf2.winfo_id()
+    #window.after(700, lambda: launch_xterm(wid_output, 900, 300, "sudo mn - c && ryu-manager --observe-links shortest_path.py")) 
 
     #stop the propagation of pack() because the outputRyuFrame resize itself as it wants. It doesn't listen to my will
     outputRyuFrame.propagate(False)
+    print(f"Questo è il PID di termf: {wid_output}")
 
 
     
     ### Bottom left corner
-    buttonFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 500, width = 800)
+    buttonFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 400, width = 700)
     buttonFrame.grid(row = 1, column = 0, sticky = 'sw')
 
     # The title of button section
@@ -182,13 +217,13 @@ def main():
     buttonFrameNested = ttk.Frame(buttonFrame, style = 'RoundedFrame.TFrame', height = 150, width = 500)
     buttonFrameNested.pack(side = 'top', expand = True, fill = 'both', anchor = 'center')
 
-    buttonTest1 = ttk.Button(buttonFrameNested, text = "Load topology #1", style = "RoundedButton.TButton")
+    buttonTest1 = ttk.Button(buttonFrameNested, text = "Load topology #1", style = "RoundedButton.TButton", command=button1)
     buttonTest1.pack(side = 'top', padx=10, pady=5, fill='x', anchor = 'center', expand=False)
 
-    buttonTest2 = ttk.Button(buttonFrameNested, text = "Load topology #2", style = "RoundedButton.TButton")
+    buttonTest2 = ttk.Button(buttonFrameNested, text = "Load topology #2", style = "RoundedButton.TButton", command=button2)
     buttonTest2.pack(side = 'top', padx=10, pady=5, fill='x', anchor = 'center', expand=False)
 
-    buttonTest3 = ttk.Button(buttonFrameNested, text = "Load topology #3", style = "RoundedButton.TButton")
+    buttonTest3 = ttk.Button(buttonFrameNested, text = "Load topology #3", style = "RoundedButton.TButton", command=button3)
     buttonTest3.pack(side = 'top', padx=10, pady=5, fill='x', expand=False)
 
     buttonFrameNested.propagate(False)
@@ -198,33 +233,34 @@ def main():
 
     ### Bottom right corner
     #Create frame that contains titleLabelTerminal for the terminal and the xterm terminal
-    termFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 500, width = 800)
+    termFrame = ttk.Frame(window, style = 'RoundedFrame.TFrame', height = 400, width = 700)
     termFrame.grid(row = 1, column = 1, sticky = 'se')
 
-    # The actual frame of the terminal
-    termf = ttk.Frame(termFrame, height=400, width=800)
-    termf.pack(side = 'bottom')
+    
 
     # The title of the terminal
     titleLabelTerminal = ttk.Label(termFrame, text="Terminal for Mininet CLI", background = "lightgray", borderwidth = 10, font=("Helvetica", 13))
     titleLabelTerminal.pack(side='top', fill = 'none', expand = False, padx = 10, pady = 5)
 
-    wid = termf.winfo_id()
+    # The actual frame of the terminal
+    termf1 = ttk.Frame(termFrame, height=400, width=700)
+    termf1.pack(side = 'bottom')
+    wid_mininet = termf1.winfo_id()
 
     # Launch xterm with specified font size and dimensions
-    window.after(700, lambda: launch_xterm(wid, 800, 400))     #With the method after() we can delay the starting of the xterminal, so the dimensions of xterm are written and the terminal can take the right size
-    print(f"Questo è il PID di termf: {wid}")
+    #window.after(700, lambda: launch_xterm(wid_mininet, 900, 300))     #With the method after() we can delay the starting of the xterminal, so the dimensions of xterm are written and the terminal can take the right size
+    print(f"Questo è il PID di termf: {wid_mininet}")
 
     termFrame.propagate(False)
 
 
 
     # Start the listening thread for the log output
-    start_listening_thread(graphLabel, outputRyuText)
+    start_listening_thread(graphLabel)
 
-    '''# Bind the window resize event to the on_resize function
-    window.bind("<Configure>", lambda event: on_resize(event, graphLabel, outputRyuText))
-    window.bind("<Configure>", lambda event: resize_terminal(event, wid, termf))'''
+    # Bind the window resize event to the on_resize function
+   # window.bind("<Configure>", lambda event: on_resize(event, graphLabel))
+   # window.bind("<Configure>", lambda event: resize_terminal(event, wid_mininet, termf1))
     
     window.mainloop()
 
